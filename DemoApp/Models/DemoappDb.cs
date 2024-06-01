0f27e2d6e3c5b7b094914c15d70cdaef1af3118d
Gayathri Dinesh <Gayathri Dinesh@LAPTOP-78VVGOQP>
2024-06-01 08:49:56 +0530

    Add project files.

---
+++ DemoApp/Models/DemoappDb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DemoApp.Models
+{
+    public class DemoappDb : DbContext
+    {
+        public DemoappDb() : base("name=MyConnectionString")
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ DemoApp/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DemoApp.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(100)]
+        public string Position { get; set; }
+
+        [StringLength(50)]
+        public string Department { get; set; }
+    }
+}
